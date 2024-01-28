87b6eda095e0b5046c64dc11e0fcefdcecf91a84
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-01-28 16:19:11 +0200

    feat: mock routes from previous app

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 @Controller('users')
@@ -6,7 +6,42 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  function() {
-    return this.usersService.sayHello();
+  sayHello() {
+    return this.usersService.sayHelloWorld();
+  }
+
+  @Post('/login')
+  login() {
+    return 'login route';
+  }
+
+  @Get('/:page')
+  getUsers() {
+    return 'get users route';
+  }
+
+  @Post('/create-account')
+  createAccount() {
+    return 'create account route';
+  }
+
+  @Post('/update-profile/:userId')
+  updateProfile() {
+    return 'update profile route';
+  }
+
+  @Delete('/delete-account/:userId')
+  deleteAccount() {
+    return 'delete account route';
+  }
+
+  @Post('/vote/:userId')
+  vote() {
+    return 'vote route';
+  }
+
+  @Post('/update-avatar')
+  updateAvatar() {
+    return 'update avatar route';
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class UsersService {
-  sayHello() {
+  sayHelloWorld() {
     return 'Hello World!';
   }
 }
