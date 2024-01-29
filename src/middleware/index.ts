d46175364f9ec9e662a443925d7562402f314f45
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-01-29 15:39:49 +0200

    refactor: middleware to functional style

--- src/middleware/index.ts
+++ src/middleware/index.ts
@@ -1,18 +1,11 @@
-import { Injectable, NestMiddleware } from '@nestjs/common';
 import { Request, Response, NextFunction } from 'express';
 
-@Injectable()
-export class isAuthorized implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('Received request... from isAuthorized');
-    next();
-  }
+export function isAuthorized(req: Request, res: Response, next: NextFunction) {
+  console.log('Received request... from isAuthorized');
+  next();
 }
 
-@Injectable()
-export class isAdmin implements NestMiddleware {
-  use(req: Request, res: Response, next: NextFunction) {
-    console.log('Received request... from isAdmin');
-    next();
-  }
+export function isAdmin(req: Request, res: Response, next: NextFunction) {
+  console.log('Received request... from isAdmin');
+  next();
 }
