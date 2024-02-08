66cb7e42e6440cd0706f260f16eeaab9815222d1
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-02-08 10:39:47 +0200

    feat: add boilerplate for the mood service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { MoodService } from './mood/mood.service';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, MoodService],
 })
 export class AppModule {}
