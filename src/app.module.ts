3cee668848d4c2c12d8af08ab2b4f84974597013
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-02-08 17:58:22 +0200

    refactor: clean moods service, as there's no need for it

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
-import { MoodsService } from './moods/mood.service';
+
 import { MoodsController } from './moods/mood.controller';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController, MoodsController],
-  providers: [AppService, MoodsService],
+  providers: [AppService],
 })
 export class AppModule {}
