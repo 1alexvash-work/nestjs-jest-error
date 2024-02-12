2400bf1dc84bcbe04ab03d50b84bcb287209cd73
Alexander Vashchuk <1alexvash.work@gmail.com>
2024-02-12 19:35:13 +0200

    feat: AuthGuard add support for JWT

--- src/helpers/AuthGuard.ts
+++ src/helpers/AuthGuard.ts
@@ -4,26 +4,29 @@ import {
   ExecutionContext,
   HttpException,
 } from '@nestjs/common';
+import jwt from 'jsonwebtoken';
 
 type TokenTypes = 'user' | 'admin';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
   async canActivate(context: ExecutionContext): Promise<boolean> {
-    const bearerToken = context.switchToHttp().getRequest()
-      .headers.authorization;
+    const request = context.switchToHttp().getRequest();
+    const token = request.headers.authorization?.split(' ')[1];
 
-    if (!bearerToken) {
-      throw new HttpException('Authorization token is missing', 401);
+    if (!token) {
+      throw new HttpException('Authorization token is not found', 401);
     }
 
-    const token: TokenTypes = bearerToken.split(' ')[1];
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_PRIVATE_KEY);
 
-    if (token !== 'user' && token !== 'admin') {
-      throw new HttpException('Authorization token is missing', 401);
+      request.user = decoded;
+
+      return true;
+    } catch (error) {
+      throw new HttpException('Invalid authorization token', 401);
     }
-
-    return true;
   }
 }
 
